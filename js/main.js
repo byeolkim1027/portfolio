db202d8726a94d63a3a0e3ffba6b1c72beecba16
Byeol <byeolkim1027@gmail.com>
2022-11-28 16:02:09 +0900

    final

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 $(document).ready(function(){
-    // const content = "안녕하세요, 웹퍼블리셔 김별입니다.";
-    // const text = document.querySelector(".txt");
-    // let i = 0;
+    "use strict";
+    const content = "안녕하세요, 웹퍼블리셔 김별입니다."
+    const text = document.querySelector(".typing")
+    let index = 0;
 
-    // function typing(){
-    //     let txt = content[i++];
-    //     text.innerHTML += txt;
-    //     if (i > content.length) {
-    //         text.textContent = "";
-    //         i = 0;
-    //     }
-    // }
+    function sleep(delay){ 
+        const start = new Date().getTime(); 
+        while (new Date().getTime() < start + delay); 
+        }
 
-    
+    function typing(){
+        text.textContent += content[index++];
+        if(index > content.length){
+            text.textContent = ""
+            index = 0;
+            sleep(1000);
+        }
+    }
+    setInterval(typing, 150)
 })
